1cbad7a91fc77ab9ec43cbd010dd2e6811436009
Ramith Chandula Weerasinghe Kulatunga <ramithchandula@gmail.com>
2024-10-27 17:49:38 -0400

    fixing cart after merge conflict error

--- components/cart.tsx
+++ components/cart.tsx
@@ -24,58 +24,42 @@ import {
 import { Label } from './ui/label';
 import { Input } from './ui/input';
 import OrderSummary from './order-summary';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
 export default function Cart() {
-    const [open, setOpen] = React.useState(false);
-    const isDesktop = useMediaQuery('(min-width: 768px)');
-    if (isDesktop) {
-        return (
-            <Dialog open={open} onOpenChange={setOpen}>
-                <DialogTrigger asChild>
-                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                        <ShoppingCart />
-                    </NavigationMenuLink>
-                </DialogTrigger>
-                <DialogContent
-                    className="sm:max-w-[425px] right-0 translate-x-0 left-auto h-screen animate-from-left
-                 data-[state=open]:slide-in-from-right-full data-[state=open]:slide-in-from-top-1/2 data-[state=open]:!zoom-in-100
-                 data-[state=closed]:slide-out-to-right-full data-[state=closed]:slide-out-to-top-1/2 data-[state=closed]:!zoom-out-100
-                 "
-                >
-                    <DialogHeader className="gap-4">
-                        <DialogTitle>Order Summary</DialogTitle>
-                        <DialogDescription>
-                            <OrderSummary className="px-4" />
-                        </DialogDescription>
-                    </DialogHeader>
-                </DialogContent>
-            </Dialog>
-        );
-    }
+  const [open, setOpen] = React.useState(false);
+  const isDesktop = useMediaQuery("(min-width: 768px)");
+  if (isDesktop) {
     return (
-        <Drawer open={open} onOpenChange={setOpen}>
-            <DrawerTrigger asChild></DrawerTrigger>
-            <DrawerContent>
-                <DrawerHeader className="text-center gap-4">
-                    <DrawerTitle className="text-center">Order Summary</DrawerTitle>
-                    <DrawerDescription>
-                        <OrderSummary className="px-4" />
-                    </DrawerDescription>
-                </DrawerHeader>
-                <DrawerFooter className="pt-2">
-                    <DrawerClose asChild>
-                        <Button variant="outline">Cancel</Button>
-                    </DrawerClose>
-                </DrawerFooter>
-            </DrawerContent>
-        </Drawer>
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogTrigger asChild>
+          <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+            <ShoppingCart />
+          </NavigationMenuLink>
+        </DialogTrigger>
+        <DialogContent className="animate-from-left left-auto right-0 h-screen translate-x-0 data-[state=closed]:!zoom-out-100 data-[state=open]:!zoom-in-100 data-[state=closed]:slide-out-to-right-full data-[state=closed]:slide-out-to-top-1/2 data-[state=open]:slide-in-from-right-full data-[state=open]:slide-in-from-top-1/2 sm:max-w-[425px]">
+          <DialogHeader className="gap-4">
+            <DialogTitle>Order Summary</DialogTitle>
+            <DialogDescription>
+              <OrderSummary className="px-4" />
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
     );
   }
   return (
     <Drawer open={open} onOpenChange={setOpen}>
       <DrawerTrigger asChild></DrawerTrigger>
       <DrawerContent>
-        <DrawerHeader className="text-left">
-          <DrawerTitle>Order Summary</DrawerTitle>
+        <DrawerHeader className="gap-4 text-center">
+          <DrawerTitle className="text-center">Order Summary</DrawerTitle>
           <DrawerDescription>
             <OrderSummary className="px-4" />
           </DrawerDescription>
